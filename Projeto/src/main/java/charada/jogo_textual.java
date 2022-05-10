251d6e251fa80de6aca781c33224d2285f450c3c
DeiveOliveira <deiveoliveira1@gmail.com>
2022-05-09 21:47:30 -0300

    Inicializei a apresentação do jogo

--- Projeto/src/main/java/charada/jogo_textual.java
+++ Projeto/src/main/java/charada/jogo_textual.java
@@ -1,10 +1,24 @@
 
 package charada;
 
+import java.util.Scanner;
+
 public class jogo_textual {
 
     public static void main(String[] args) {
-        System.out.println("teste");
+        Scanner input = new Scanner (System.in);
+        String  nome;
+        System.out.println("'A Charada'. Um jogo RPG Textual.");
+        System.out.println("Idealizado e Desenvolvido por : Alesandro da Rocha; Augusto Brito; Brenno Lima e Deive Oliveira. Alunos do Curso Analise e Desenvolvimento de Sistemas.");
+        System.out.println("Digite o nome do personagem: ");
+        nome = input.next();
+        System.out.println("Bem vindo ao jogo " + nome + "\n O jogo acontece no espaço do Senac, do qual você irá ter interações com o espaço da Universidade.");
+        System.out.println("Será apresentado charadas a você, os seus resultados irão definir o seu futuro. \nDica: usufrua do espaço, para interagir com o jogo");
+        
+        System.out.println("----- MENU -----");
+        System.out.println("Digite 1 para jogar");
+        System.out.println("Digite 2 para rever as instruções");
+
     }
     
 }
